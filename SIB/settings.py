72a51e31960e3458212b48c19f43633554315e94
aidatak97 <40061242+aidatak97@users.noreply.github.com>
2022-03-13 20:26:28 +0100

    Guessing task: control group

--- SIB/settings.py
+++ SIB/settings.py
@@ -16,40 +16,40 @@ SESSION_CONFIG_DEFAULTS = dict(
     real_world_currency_per_point=1.00, participation_fee=4.00, doc=""
 )
 
-SESSION_CONFIGS = [dict(
-        name='control',
+SESSION_CONFIGS = [
+    dict(
+        name="control",
         display_name="SIB_control",
-        num_demo_participants=10,
-        app_sequence=['Intro_all',
-                      'SIM',
-                      'GuessingTask',
-                      'Payout'
-                      ],),
-        dict(name='SI',
+        num_demo_participants=5,
+        app_sequence=[  #'Intro_all',
+            #'SIM',
+            "GuessingTask",
+            #'Payout'
+        ],
+    ),
+    dict(
+        name="SI",
         display_name="SIB_SI",
         num_demo_participants=10,
-        app_sequence=['Intro_all',
-                      'SIM',
-                      'GuessingTask',
-                      'Payout'
-                      ],
-)]
+        app_sequence=["Intro_all", "SIM", "GuessingTask", "Payout"],
+    ),
+]
 
 PARTICIPANT_FIELDS = []
 SESSION_FIELDS = []
 
 # ISO-639 code
 # for example: de, fr, ja, ko, zh-hans
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = "en"
 
 # e.g. EUR, GBP, CNY, JPY
-REAL_WORLD_CURRENCY_CODE = 'USD'
+REAL_WORLD_CURRENCY_CODE = "USD"
 USE_POINTS = True
 
-ADMIN_USERNAME = 'admin'
+ADMIN_USERNAME = "admin"
 # for security, best to set admin password in an environment variable
-ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+ADMIN_PASSWORD = environ.get("OTREE_ADMIN_PASSWORD")
 
 DEMO_PAGE_INTRO_HTML = """ """
 
-SECRET_KEY = '2646253538807'
+SECRET_KEY = "2646253538807"
